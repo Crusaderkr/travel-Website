271de10f8873acc0df2e6bb09b214c5ec11c4cf3
Kapil Rawat <70755182+Crusaderkr@users.noreply.github.com>
2023-06-15 16:43:48 +0530

    Add files via upload

---
+++ haridwar/haridwar.js
@@ -0,0 +1,18 @@
+function openBooking(){
+   window.location.href="/booking.html";
+};
+
+for (let index = 0; index <1100; index++) {
+   setInterval(() => {
+      window.scroll(0,index);
+   }, 5000);
+}
+ 
+
+
+
+
+   
+
+
+  
